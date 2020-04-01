24e27ec1a8243ae834f92cc617224e846b31a923
Kosmo_11 <Kosmo_11@DESKTOP-3T7EHNA>
2020-04-01 14:19:34 +0900

    0401

--- src/ver08/PhoneBookManager.java
+++ src/ver08/PhoneBookManager.java
@@ -7,13 +7,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 
-/*
- 7단계[컬렉션] : PhoneBookVer07.java
-동명이인이 있다고 가정했을때 3단계에서 진행한 배열을 이용해서는 구조적으로 중복을 막을수 없다.
-이번에는 중복 저장을 허용하지 않는 HashSet<E> 클래스를 대상으로 저장이 되도록 해보자. 
-“이름이 같으면 다른 정보가 달라도 동일한 객체(인스턴스)로 간주한다”
-즉 동일한 이름을 입력하는 경우라면 덮어쓸지 여부를 물어본후 처리한다.
- */
 public class PhoneBookManager {
 	
 	HashSet<PhoneInfo>myInfo; 
--- src/ver08/PhoneInfo.java
+++ src/ver08/PhoneInfo.java
@@ -4,11 +4,10 @@ import java.io.Serializable;
 import java.util.Scanner;
 
 public class PhoneInfo implements Serializable{
-	//멤버변수
+	
 	public String name;
 	public String phoneNumber;
 	
-	//생성자
 	public PhoneInfo(String name, String phoneNumber) {
 		this.name = name;
 		this.phoneNumber = phoneNumber;
@@ -23,7 +22,6 @@ public class PhoneInfo implements Serializable{
 		this.name = name;
 	}
 
-	//정보 출력용 메소드
 	public void showPhoneInfo() {
 		System.out.println("이름:"+name);
 		System.out.println("전화번호:"+phoneNumber);
